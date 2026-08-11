604d4e878e0f4ee1fbaa8a8a162a174e8dbb189a
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-08-11 13:58:37 +1000

    feat(fit3143): wk02 task6 string.h demo and own string functions

--- fit3143/wk02/applied02/task6.c
+++ fit3143/wk02/applied02/task6.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
+int my_strlen(char *s);
+void my_strcat(char *dest, char *src);
+void reverse(char *s);
 
 int main()
 {
-	char text_1[100], text_2[100], text_3[100];
+	// Part 1: string copying the hard way
+
+	char text_1[100], text_2[100];
 	char *ta, *tb;
 	int i;
 
@@ -12,7 +19,7 @@ int main()
 
 	// Copying the message to text_1 the hard way: array indexing
 	i = 0;
-	while ((text_1[i] = message[i]) != "\0") {
+	while ((text_1[i] = message[i]) != '\0') {
 		i++;
 	}
 	printf("Text_1: %s\n", text_1);
@@ -20,9 +27,117 @@ int main()
 	// Copy using explicit pointer
 	ta = message;
 	tb = text_2;
-	while ((*tb++ = *ta++) != "\0")
+	while ((*tb++ = *ta++) != '\0')
 		;
 	printf("Text_2: %s\n", text_2);
 
+	/*
+	The assignment inside the while loop conditions also work as loop tests.
+
+	We do this until we hit "\0", this is the termination character every string
+	has.
+	*/
+
+	// Part 2: Using string.h
+
+	char line[100], *sub_text;
+
+	// copying string to another (dest, source)
+	strcpy(line, "hello, I am a string");
+	printf("Line: %s\n", line);
+
+	// string concatenation
+	strcat(line, " what are you");
+	printf("Line: %s\n", line);
+
+	// string length
+	printf("Length of line: %d\n", (int)strlen(line));
+
+	// portion of the string where the first occurrence of W is found
+	// expected output: Nothing, there's no characters with uppercase W.
+	if ((sub_text = strchr(line, 'W')) != NULL)
+		printf("String starting with \"W\" ->%s\n", sub_text);
+
+	// expected output: "what are you".
+	if ((sub_text = strchr(line, 'w')) != NULL)
+		printf("String starting with \"w\" ->%s\n", sub_text);
+
+	// expected output: "u", just the last character
+	if ((sub_text = strchr(line, 'u')) != NULL)
+		printf("String starting with \"u\" ->%s\n", sub_text);
+
+	// Part 3: testing
+
+    // my_strlen
+	if (strlen("test string") == my_strlen("test string"))
+		printf("my_strlen pass\n");
+
+    // my_strcat
+    char line_2[100];
+    strcpy(line_2, "Hello this is another string");
+    printf("Line: %s\n", line_2);
+    my_strcat(line_2, " what is this");
+    printf("Line: %s\n", line_2);
+
+    // reverse
+    char example[] = "hello";
+    reverse(example);
+    printf("Reversed \"hello\": %s\n", example);
+
+    char example2[] = "hell";
+    reverse(example2);
+    printf("Reversed \"hell\": %s\n", example2);
+
 	return 0;
 }
+
+// Part 3: creating my own
+
+int my_strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return i;
+}
+
+void my_strcat(char *dest, char *src) {
+    // find the end of dest
+    int i = 0;
+    while (dest[i] != '\0') i++;
+
+    // then do the copy loop to concat
+    int k = 0;
+    while (src[k] != '\0') {
+        dest[i] = src[k];
+        ++i;
+        ++k;
+    }
+    dest[i] = '\0';
+
+    return;
+}
+
+void reverse(char *s) {
+    int len = my_strlen(s);
+    int k = len-1;
+    int i = 0;
+    while (i < k) {
+        char temp = s[i];
+        s[i] = s[k];
+        s[k] = temp;
+        i++;
+        k--;
+    }
+    return;
+}
+
+/*
+hello
+^   ^
+oellh
+ ^ ^
+olleh
+  ^
+  ^
+*/
