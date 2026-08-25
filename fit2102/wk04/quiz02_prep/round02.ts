7a30ad8fea4af92fc395523cebf3c3d49747a18b
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-08-25 21:49:00 +1000

    feat(fit2102): add quiz 2 prep round 2 on higher-order functions

---
+++ fit2102/wk04/quiz02_prep/round02.ts
@@ -0,0 +1,27 @@
+// Higher-order functions and combinators
+
+//
+// Q7)
+// Define compose and flip in curried arrow form. Give TypeScript types for
+// both.
+
+// f => g => x => f(g(x))
+// f takes Y and returns Z
+// g takes X and returns Y
+const compose =
+  <Y, Z>(f: (y: Y) => Z) =>
+  <X>(g: (x: X) => Y) =>
+  (x: X): Z =>
+    f(g(x));
+
+// a => b => f(a)(b)
+// f takes a and b curried, returning C
+const flip =
+  <A, B, C>(f: (a: A) => (b: B) => C) =>
+  (a: A) =>
+  (b: B): C =>
+    f(a)(b);
+
+// 
+// Q8)
+// 
