adeee1ba2e586ca3900a72ecb99100aacd6af791
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-08-25 18:36:14 +1000

    feat(fit2102): add quiz 2 prep question on free and bound variables

--- fit2102/wk04/quiz02_prep/round01.js
+++ fit2102/wk04/quiz02_prep/round01.js
@@ -1,5 +1,6 @@
 const log = console.log;
 
+// Q1)
 // Rewrite 
 // function add(a, b) { 
 //    return a + b;
@@ -11,3 +12,10 @@ const add = a => b => a + b;
 // add(3)(3) = 6, works. But... add(3) becomes a closure that remembers the 
 // value 3 + b as an anonymous function. So this allows us to do things like
 // const addThree = add(3), then call addThree(5) = 8...
+
+
+// Q2)
+// Name the free and bound variables in x => y => x + y + z
+
+// x and y are bound variables because they are passed in as parameters.
+// z is a free variable from the outer scope via a closure.
