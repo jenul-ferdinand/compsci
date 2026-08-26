d18cc08368c8f284d8224e252d4428ae0f557753
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-08-26 14:19:31 +1000

    feat(fit3184): serve handbook endpoint for any unit code

--- fit3184/wk05/course-api/main.py
+++ fit3184/wk05/course-api/main.py
@@ -8,12 +8,20 @@ COUNTER_FILE = "counter.txt"
 def get_root():
     return { "message": "This is the root endpoint of the API." }
 
-@app.get("/api/v1/courses")
-def get_courses():
+def get_counter(should_increment: bool = False):
     count = 0
     if os.path.exists(COUNTER_FILE):
-        with open(COUNTER_FILE, "r") as f:
+        with open(COUNTER_FILE, 'r') as f:
             count = f.read().strip()
+    if should_increment:
+        count += 1
+        with open(COUNTER_FILE, 'w') as f:
+            f.write(str(count))
+    return count
+
+@app.get("/api/v1/courses")
+def get_courses():
+    count = get_counter(should_increment=False)
     return {
         "courses": [
             { "id": "FIT3184", "name": "Cloud Computing"}
@@ -21,23 +29,17 @@ def get_courses():
         "message": f"The number of students visited the handbook is {count}."
     }
 
-@app.get("/api/v1/fit3184")
-def fit3184():
+@app.get("/api/v1/{unitcode}")
+def unit(unitcode: str):
     try:
-        count = 0
-        if os.path.exists(COUNTER_FILE):
-            with open(COUNTER_FILE, "r") as f:
-                count = int(f.read().strip())
-        count += 1
-        with open(COUNTER_FILE, "w") as f:
-            f.write(str(count))
+        count = get_counter(should_increment=True)
         return {
-            "message": f"This is the FIT3184 handbook, you are {count} visitor."
+            "message": f"This is the {unitcode.upper()} handbook, you are {count} visitor"
         }
     except HTTPException as e:
         raise HTTPException(
-            status_code=500, 
-            detail=f"Error accessing counter: {str(e)}"
+            status_code=500,
+            detail=f'Error accessing counter: {str(e)}'
         )
 
 # run dis with uvicorn main:app --host 0.0.0.0 --reload --port 8080
