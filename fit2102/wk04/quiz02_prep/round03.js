fae68d6ecf33eed020796e3aaa0ff6883781dd0c
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-08-26 20:55:51 +1000

    feat(fit2102): add cons list runtime explanation

---
+++ fit2102/wk04/quiz02_prep/round03.js
@@ -0,0 +1,12 @@
+
+//
+// Q15)
+// In cons = x => y => f => f(x)(y), what is a cons list made of at runtime?
+// No arrays, no objects. What actually holds the data?
+
+// Let's beta reduce this one.
+// cons = x => [y => f => f(x)(y)]
+// cons(1) = y => [f => f(1)(y)]
+// cons(1)(2) = f => f(1)(2)
+// This means it's a closure as a function value. The values in this example, 1
+// and 2 live in the captured environment record.
